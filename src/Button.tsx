c1781f9467ead310b07de1381169fc67f241237c
Or <60711866+oregev@users.noreply.github.com>
2021-12-06 19:53:52 +0200

    feat: add button component

---
+++ src/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface Props {
+  size: string;
+}
+
+// eslint-disable-next-line react/function-component-definition
+export const Button: React.FC<Props> = ({ size }) => {
+  const rightSize = size ? 'big' : 'small';
+  return <button type="button">{rightSize}</button>;
+};
